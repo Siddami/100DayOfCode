4db98aa8768c8e586dec2b95f33b6fd43842c2c6
Siddami <dharmmy621@gmail.com>
2024-05-03 17:34:32 +0100

    variable.js

---
+++ variable.js
@@ -0,0 +1,8 @@
+//.4 Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let theString = "Hello";
+let booleanType = true;
+let notDefined;
+let nullType = null;
+let numType = 3;
+

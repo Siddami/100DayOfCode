790512606a02448ea4b0059a7ea80934b3a2e766
Siddami <dharmmy621@gmail.com>
2024-05-07 21:40:00 +0100

    level1.js

--- day7/level1.js
+++ day7/level1.js
@@ -1,111 +1,111 @@
-// //1. Iterate 0 to 10 using for loop, do the same using while and do while loop
+//1. Iterate 0 to 10 using for loop, do the same using while and do while loop
 
-// //for loop
+//for loop
 
-// for(let i = 0; i <= 10; i++){
-//     console.log(i)
-// }
+for(let i = 0; i <= 10; i++){
+    console.log(i)
+}
 
-// //while loop
+//while loop
 
-// let num = 0;
+let num = 0;
 
-// while(num <= 10){
-//     console.log(num)
-//     num++;
-// }
+while(num <= 10){
+    console.log(num)
+    num++;
+}
 
-// //do while loop
-// let no = 0 ;
+//do while loop
+let no = 0 ;
 
-// do {
-//     console.log(no)
-//     no++
-// }while(no <= 10)
+do {
+    console.log(no)
+    no++
+}while(no <= 10)
 
 
 
-// //2. Iterate 10 to 0 using for loop, do the same using while and do while loop
+//2. Iterate 10 to 0 using for loop, do the same using while and do while loop
 
-// //for loop
+//for loop
 
-// for(let rev = 10; rev >=0; rev--){
-//     console.log(rev)
-// }
+for(let rev = 10; rev >=0; rev--){
+    console.log(rev)
+}
 
-// //while loop
+//while loop
 
-// let reve = 10;
+let reve = 10;
 
-// while(reve >= 0){
-//     console.log(reve)
-//     reve--;
-// }
+while(reve >= 0){
+    console.log(reve)
+    reve--;
+}
 
-// //do while loop
+//do while loop
 
-// let reverse = 10
+let reverse = 10
 
-// do{
-//     console.log(reverse)
-//     reverse--
-// }while(reverse >= 0)
+do{
+    console.log(reverse)
+    reverse--
+}while(reverse >= 0)
 
-// //3. Iterate 0 to n using for loop
-// let n = 20;
-// for(let j = 0; j <= n; j++){
-//     console.log(j)
-// }
+//3. Iterate 0 to n using for loop
+let n = 20;
+for(let j = 0; j <= n; j++){
+    console.log(j)
+}
 
-// //4. Write a loop that makes the following pattern using console.log():
+//4. Write a loop that makes the following pattern using console.log():
 
-// let hash = '#';
+let hash = '#';
 
-// for(let a = 0; a <= 7; a++){
-//     console.log(hash)
-//     hash += '#'
-// }
+for(let a = 0; a <= 7; a++){
+    console.log(hash)
+    hash += '#'
+}
 
-// //5. Use loop to print the following pattern:
-// /*0 x 0 = 0
-// 1 x 1 = 1
-// 2 x 2 = 4
-// 3 x 3 = 9
-// 4 x 4 = 16
-// 5 x 5 = 25
-// 6 x 6 = 36
-// 7 x 7 = 49
-// 8 x 8 = 64
-// 9 x 9 = 81
-// 10 x 10 = 100*/
+//5. Use loop to print the following pattern:
+/*0 x 0 = 0
+1 x 1 = 1
+2 x 2 = 4
+3 x 3 = 9
+4 x 4 = 16
+5 x 5 = 25
+6 x 6 = 36
+7 x 7 = 49
+8 x 8 = 64
+9 x 9 = 81
+10 x 10 = 100*/
 
-// for(let k = 0; k <= 10; k++){
-//     console.log(`${k} * ${k} = ${k * k}`)
-// }
+for(let k = 0; k <= 10; k++){
+    console.log(`${k} * ${k} = ${k * k}`)
+}
 
-// //6. Using loop print the following pattern
-// /* i    i^2   i^3
-//  0    0     0
-//  1    1     1
-//  2    4     8
-//  3    9     27
-//  4    16    64
-//  5    25    125
-//  6    36    216
-//  7    49    343
-//  8    64    512
-//  9    81    729
-//  10   100   1000*/
+//6. Using loop print the following pattern
+/* i    i^2   i^3
+ 0    0     0
+ 1    1     1
+ 2    4     8
+ 3    9     27
+ 4    16    64
+ 5    25    125
+ 6    36    216
+ 7    49    343
+ 8    64    512
+ 9    81    729
+ 10   100   1000*/
 
-//  for(let m = 0; m <= 10; m++){
-//     console.log(`${m} ${m ** 2} ${m ** 3}`)
-//  }
+ for(let m = 0; m <= 10; m++){
+    console.log(`${m} ${m ** 2} ${m ** 3}`)
+ }
 
-//  //7. Use for loop to iterate from 0 to 100 and print only even numbers
+ //7. Use for loop to iterate from 0 to 100 and print only even numbers
 
-//  for(let even = 0; even <= 100; even+2){
-//     console.log(even)
-//  }
+ for(let even = 0; even <= 100; even+2){
+    console.log(even)
+ }
 
 //8. Use for loop to iterate from 0 to 100 and print only odd numbers
 for(let odd = 0; odd <= 100; odd++){
